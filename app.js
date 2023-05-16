610fc9846bf2adc687e66923ccbcb98d1a102fcc
Willeboii717 <68202860+Willeboii717@users.noreply.github.com>
2023-05-16 21:46:25 +0200

    Routing added
    Made Routing work

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var specificViewRouter = require('./routes/specificView');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,7 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/index', indexRouter);
+app.use('/specificView', specificViewRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
