9e08f26f6ef5125c352e08fd6560698fd7d711dc
Willeboii717 <68202860+Willeboii717@users.noreply.github.com>
2023-05-18 19:20:27 +0200

    added search (not working)

--- public/javascripts/button.js
+++ public/javascripts/button.js
@@ -1,4 +1,24 @@
+document.getElementById('searchbar').addEventListener('input', search);
 
-function logConsoleButton() {
-    console.log("Hello Button");
+function search(){
+    //console.log(document.getElementById('searchbar').value)
+    var searchinput = document.getElementById('searchbar').value
+    //console.log (typeof searchinput)
+    //console.log (searchinput)
+    searchinput = searchinput.toLowerCase()
+    const allpictures = document.getElementsByClassName('pictures')
+    
+    for(let i = 0;i<allpictures.length;i++){
+        var comparethis = allpictures[i].src
+
+    
+        if (comparethis.includes(searchinput)){
+            console.log('match')
+            allpictures[i].style.display = '';
+
+        }
+        else {
+            allpictures[i].style.display = 'none'
+        }
+    }
 }
