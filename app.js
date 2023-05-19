d7470f2c3495d5632a7afd14a5e399c57a67f685
Willeboii717 <68202860+Willeboii717@users.noreply.github.com>
2023-05-19 23:00:45 +0200

    Readded logging -_-

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 var mysql = require('mysql');
 var fs = require("fs");
 
@@ -13,9 +15,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 //Egna Moduler
 //var query = require("./queryHandler");
