fdf3ff0f358ce6af38ffe0a8b8688c9171d64d8d
Willeboii717 <68202860+Willeboii717@users.noreply.github.com>
2023-05-21 04:28:34 +0200

    .

--- routes/testRouter.js
+++ routes/testRouter.js
@@ -9,19 +9,31 @@ router.get('/searchbar', async function(req, res, next) {
     var sql = "CALL searchImagebyCategory(?)";
     buffer = await db(sql, search);
     let data = (JSON.stringify(buffer)) ;
-    data = JSON.parse(data);
+    data = JSON.parse(data); /*
     for (var i = 0; i<data.length; i++) {
-        var slot = data[0][i];
-        if (slot?.["Titel"] != undefined) {
-            res.render('index', {image1: slot["Titel"]});
-        }
-        else {
-            res.render('index', {image1: "Inget Resultat"});
-        }
+        var slot = [];
+        console.log("Data: " + data[0][i]);
+        slot[i] = data[0][i];
+        console.log("Slot: " + slot[i]);
     }
+    if (slot?.["Titel"] != undefined) {
+        res.render('index', {
+                array: 
+                slot[
+                "test1",
+                "test2" ],
+                test: "hello"
+        });
+    }
+    else {
+        res.render('index', {test: "Inget Resultat"});
+    }
+    */
+   res.render('index', {array: slot = ["test1", "test2", "test3", "test4", "test5", "test6"]});
 });
 
 module.exports = router;
 
-//Sätt in data i Jade variabel
-//Sätt in data i länk
+//Skicka array av SQL resultat
+//Kolla om det finns ett värde
+//Sätt in värdet
